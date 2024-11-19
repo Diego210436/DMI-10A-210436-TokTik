d8a6c46a4db2e5c362115cd48657bd4ca2965d4d
Diego <210436@utxicotepec.edu.mx>
2024-11-19 08:33:25 -0600

    Creacion Inicial del proyecto Flutter

---
+++ toktik_210436/android/app/src/main/kotlin/com/example/toktik_210436/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.toktik_210436
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ toktik_210436/lib/config/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData getTime() => ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark
+  );
+}
